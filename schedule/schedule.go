63247c8c07705725a62b1ff9857ba3326be80fed
Travis Tomsu <ttomsu@google.com>
2020-03-26 11:47:04 -0400

    Add non-proto based structs libraries for easier use and readability

---
+++ schedule/schedule.go
@@ -0,0 +1,165 @@
+package schedule
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+const (
+	DateFormat = "Mon 02 Jan 2006"
+)
+
+// Schedule represents a series of Shifts, in temporal order.
+type Schedule struct {
+	// Shifts is the list of shifts in order. The last Shift, and only the last Shift, should have a StopTime value.
+	Shifts []*Shift `json:"shifts"`
+}
+
+func (sch *Schedule) LastShift() *Shift {
+	if sch.Shifts == nil || len(sch.Shifts) == 0 {
+		return nil
+	}
+	return sch.Shifts[len(sch.Shifts)-1]
+}
+
+func (sch *Schedule) Validate() error {
+	if sch == nil {
+		return fmt.Errorf("schedule cannot be nil")
+	}
+
+	if sch.Shifts == nil || len(sch.Shifts) == 0 {
+		fmt.Println("No input shifts found. Starting from scratch.")
+		return nil
+	}
+
+	var previousStartDate time.Time
+	for i, shift := range sch.Shifts {
+		if err := shift.Validate(); err != nil {
+			return fmt.Errorf("schedule is invalid because shift %v is invalid: %v", i, err)
+		}
+
+		if !previousStartDate.IsZero() && previousStartDate.After(shift.StartDate) {
+			return fmt.Errorf("shifts out of order: shift time %v found after %v", previousStartDate, shift.StartDate)
+		}
+		previousStartDate = shift.StartDate
+
+		if shift == sch.LastShift() {
+			if shift.StopDate.IsZero() {
+				return fmt.Errorf("missing stop date on last shift")
+			}
+		} else {
+			// All other shifts should not have a stop date.
+			if !shift.StopDate.IsZero() {
+				return fmt.Errorf("stop date is only valid on the last shift. Found stop date %v on shift at index %v", shift.StopDate, i)
+			}
+		}
+	}
+
+	return nil // It's all good.
+}
+
+// A shift represents a span of time someone is on duty.
+type Shift struct {
+	// The start date, inclusive, of when the User goes on duty. Only year, month, and day field are relevant.
+	StartDate time.Time `json:"startDate,omitempty"`
+	User      string    `json:"user,omitempty"`
+
+	// UserOverride should be used when Users want to swap or alter shift assignments without disturbing the normal
+	// rotation cycle.
+	UserOverride string `json:"userOverride,omitempty"`
+
+	// StopDate is inclusive, and must only be used on the last Shift of a Schedule. For all other Shifts, the stop date
+	// is implied by the next shift's StartDate, and this value should remain the zero `time.Time` value.
+	StopDate time.Time `json:"stopDate,omitempty"`
+}
+
+// StopDateExclusive is used when an API uses (start, stop) date ranges as (inclusive, exclusive). May returns a zero
+// `time.Time` when stop is not set.
+func (sh *Shift) StopDateExclusive() time.Time {
+	if sh.StopDate.IsZero() {
+		return sh.StopDate // zero value is both inclusive and exclusive.
+	}
+	return sh.StopDate.Add(24 * time.Hour)
+}
+
+// StopDateExclusive can be used when an API uses (start, stop) date ranges as (inclusive, exclusive).
+func (sh *Shift) SetStopDateExclusive(excl time.Time) {
+	if excl.IsZero() {
+		sh.StopDate = excl // zero value is both inclusive and exclusive.
+	}
+	sh.StopDate = excl.Add(-24 * time.Hour)
+}
+
+func (sh *Shift) ClearStopDate() {
+	sh.StopDate = time.Time{}
+}
+
+func (sh *Shift) Validate() error {
+	if sh == nil {
+		return fmt.Errorf("shift cannot be nil")
+	}
+
+	if sh.User == "" {
+		return fmt.Errorf("user cannot be empty")
+	}
+
+	if sh.StartDate.IsZero() {
+		return fmt.Errorf("start date cannot be zero value")
+	}
+
+	if !sh.StopDate.IsZero() && sh.StartDate.After(sh.StopDate) {
+		return fmt.Errorf("start date must be before stop date")
+	}
+
+	return nil
+}
+
+// MarshalJSON returns timestamps in the `DateFormat` format.
+func (sh *Shift) MarshalJSON() ([]byte, error) {
+	// Technique borrowed from http://choly.ca/post/go-json-marshalling/
+	type Alias Shift
+
+	aux := &struct {
+		*Alias
+		StartDate string `json:"startDate"`
+		StopDate  string `json:"stopDate,omitempty"`
+	}{
+		Alias:     (*Alias)(sh),
+		StartDate: sh.StartDate.Format(DateFormat),
+	}
+
+	if !sh.StopDate.IsZero() {
+		aux.StopDate = sh.StopDate.Format(DateFormat)
+	}
+
+	return json.Marshal(aux)
+}
+
+// UnmarshalJSON reads timestamps in the `DateFormat` format, and will throw parsing error otherwise.
+func (sh *Shift) UnmarshalJSON(data []byte) error {
+	// Technique borrowed from http://choly.ca/post/go-json-marshalling/
+	type Alias Shift
+	aux := &struct {
+		*Alias
+		StartDate string `json:"startDate"`
+		StopDate  string `json:"stopDate,omitempty"`
+	}{
+		Alias: (*Alias)(sh),
+	}
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	var err error
+	if sh.StartDate, err = time.Parse(DateFormat, aux.StartDate); err != nil {
+		return fmt.Errorf("erroring parsing start date: %v", err)
+	}
+	if aux.StopDate != "" {
+		if sh.StopDate, err = time.Parse(DateFormat, aux.StopDate); err != nil {
+			return fmt.Errorf("erroring parsing stop date: %v", err)
+		}
+	}
+
+	return nil
+}
---
+++ schedule/schedule_test.go
@@ -0,0 +1,395 @@
+package schedule
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/ghodss/yaml"
+)
+
+func TestScheduleValidate(t *testing.T) {
+	for _, tc := range []struct {
+		desc     string
+		schedule *Schedule
+		wantErr  bool
+	}{
+		{
+			desc:     "nil schedule",
+			schedule: nil,
+			wantErr:  true,
+		},
+		{
+			desc:     "empty schedule",
+			schedule: &Schedule{},
+		},
+		{
+			desc: "empty shifts",
+			schedule: &Schedule{
+				Shifts: make([]*Shift, 1),
+			},
+			wantErr: true,
+		},
+		{
+			desc: "invalid shift",
+			schedule: &Schedule{
+				Shifts: []*Shift{
+					{}, // shift won't validate
+				},
+			},
+			wantErr: true,
+		},
+		{
+			desc: "shifts out of order",
+			schedule: &Schedule{
+				Shifts: []*Shift{
+					{
+						User:      "bar",
+						StartDate: time.Date(2020, 6, 8, 0, 0, 0, 0, time.UTC),
+					},
+					{
+						User:      "foo",
+						StartDate: time.Date(2020, 6, 1, 0, 0, 0, 0, time.UTC),
+						StopDate:  time.Date(2020, 6, 13, 0, 0, 0, 0, time.UTC),
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			desc: "stop on non-last shift",
+			schedule: &Schedule{
+				Shifts: []*Shift{
+					{
+						User:      "bar",
+						StartDate: time.Date(2020, 6, 8, 0, 0, 0, 0, time.UTC),
+						StopDate:  time.Date(2020, 6, 13, 0, 0, 0, 0, time.UTC),
+					},
+					{
+						User:      "foo",
+						StartDate: time.Date(2020, 6, 1, 0, 0, 0, 0, time.UTC),
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			desc: "missing stop on last shift",
+			schedule: &Schedule{
+				Shifts: []*Shift{
+					{
+						User:      "foo",
+						StartDate: time.Date(2020, 6, 1, 0, 0, 0, 0, time.UTC),
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			desc: "valid schedule",
+			schedule: &Schedule{
+				Shifts: []*Shift{
+					{
+						User:      "foo",
+						StartDate: time.Date(2020, 6, 1, 0, 0, 0, 0, time.UTC),
+					},
+					{
+						User:      "bar",
+						StartDate: time.Date(2020, 6, 8, 0, 0, 0, 0, time.UTC),
+						StopDate:  time.Date(2020, 6, 14, 0, 0, 0, 0, time.UTC),
+					},
+				},
+			},
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			err := tc.schedule.Validate()
+
+			if tc.wantErr && err == nil {
+				t.Errorf("err expected and not received.")
+				return
+			} else if !tc.wantErr && err != nil {
+				t.Errorf("got unexpected error: %v:", err)
+				return
+			} else if tc.wantErr {
+				// Successfully invoked error condition
+				return
+			}
+		})
+	}
+}
+
+func TestMarshalSchedule(t *testing.T) {
+	for _, tc := range []struct {
+		desc     string
+		schedule *Schedule
+		want     string
+	}{
+		{
+			desc: "valid",
+			schedule: &Schedule{
+				Shifts: []*Shift{
+					{
+						User:         "foo",
+						UserOverride: "baz",
+						StartDate:    time.Date(2020, 6, 1, 0, 0, 0, 0, time.UTC),
+					},
+					{
+						User:      "bar",
+						StartDate: time.Date(2020, 6, 8, 0, 0, 0, 0, time.UTC),
+						StopDate:  time.Date(2020, 6, 14, 0, 0, 0, 0, time.UTC),
+					},
+				},
+			},
+			want: `shifts:
+- startDate: Mon 01 Jun 2020
+  user: foo
+  userOverride: baz
+- startDate: Mon 08 Jun 2020
+  stopDate: Sun 14 Jun 2020
+  user: bar
+`,
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			got, err := yaml.Marshal(tc.schedule)
+			if err != nil {
+				t.Errorf("marshal schedule error: %v", err)
+				return
+			}
+
+			gotString := string(got)
+			if tc.want != gotString {
+				t.Errorf("want:\n%v\n\ngot:\n%v", tc.want, gotString)
+			}
+		})
+	}
+}
+
+func TestUnmarshalSchedule(t *testing.T) {
+	for _, tc := range []struct {
+		desc     string
+		schedule string
+		want     *Schedule
+	}{
+		{
+			desc: "valid",
+			schedule: `shifts:
+- startDate: Mon 01 Jun 2020
+  user: foo
+  userOverride: baz
+- startDate: Mon 08 Jun 2020
+  stopDate: Sun 14 Jun 2020
+  user: bar
+`,
+			want: &Schedule{
+				Shifts: []*Shift{
+					{
+						User:         "foo",
+						UserOverride: "baz",
+						StartDate:    time.Date(2020, 6, 1, 0, 0, 0, 0, time.UTC),
+					},
+					{
+						User:      "bar",
+						StartDate: time.Date(2020, 6, 8, 0, 0, 0, 0, time.UTC),
+						StopDate:  time.Date(2020, 6, 14, 0, 0, 0, 0, time.UTC),
+					},
+				},
+			},
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			got := &Schedule{}
+			err := yaml.Unmarshal([]byte(tc.schedule), got)
+			if err != nil {
+				t.Fatalf("unmarshal error: %v", err)
+			}
+
+			if !reflect.DeepEqual(*tc.want, *got) {
+				t.Errorf("schedules are not deep equal. want:\n%v\n\ngot:%v", tc.want, got)
+			}
+		})
+	}
+}
+
+func TestMarshalShift(t *testing.T) {
+	for _, tc := range []struct {
+		desc    string
+		shift   *Shift
+		wantErr bool
+		want    string
+	}{
+		{
+			desc: "valid",
+			shift: &Shift{
+				StartDate:    time.Date(2020, 6, 1, 0, 0, 0, 0, time.UTC),
+				StopDate:     time.Date(2020, 6, 1, 0, 0, 0, 0, time.UTC),
+				User:         "foo",
+				UserOverride: "bar",
+			},
+			want: `startDate: Mon 01 Jun 2020
+stopDate: Mon 01 Jun 2020
+user: foo
+userOverride: bar
+`,
+		},
+		{
+			desc: "optional fields omitted",
+			shift: &Shift{
+				StartDate: time.Date(2020, 6, 1, 0, 0, 0, 0, time.UTC),
+				User:      "foo",
+			},
+			want: `startDate: Mon 01 Jun 2020
+user: foo
+`,
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			got, err := yaml.Marshal(tc.shift)
+			if err != nil {
+				t.Errorf("marshal shift error: %v", err)
+				return
+			}
+
+			gotString := string(got)
+			if tc.want != gotString {
+				t.Errorf("want:\n%v\n\ngot:\n%v", tc.want, gotString)
+			}
+		})
+	}
+}
+
+func TestUnmarshalShift(t *testing.T) {
+	for _, tc := range []struct {
+		desc    string
+		shift   string
+		wantErr bool
+		want    Shift
+	}{
+		{
+			desc: "valid",
+			shift: `startDate: Mon 01 Jun 2020
+stopDate: Mon 01 Jun 2020
+user: foo
+userOverride: bar
+`,
+			want: Shift{
+				StartDate:    time.Date(2020, 6, 1, 0, 0, 0, 0, time.UTC),
+				StopDate:     time.Date(2020, 6, 1, 0, 0, 0, 0, time.UTC),
+				User:         "foo",
+				UserOverride: "bar",
+			},
+		},
+		{
+			desc: "optional fields omitted",
+			shift: `startDate: Mon 01 Jun 2020
+user: foo
+`,
+			want: Shift{
+				StartDate: time.Date(2020, 6, 1, 0, 0, 0, 0, time.UTC),
+				User:      "foo",
+			},
+		},
+		{
+			desc:    "invalid start date",
+			wantErr: true,
+			shift: `startDate: Monday 01 Jun 2020
+user: foo
+`,
+		},
+		{
+			desc:    "invalid stop date",
+			wantErr: true,
+			shift: `startDate: Mon 01 Jun 2020
+stopDate: Monday 01 Jun 2020
+user: foo
+`,
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			got := &Shift{}
+			err := yaml.Unmarshal([]byte(tc.shift), got)
+
+			if tc.wantErr && err == nil {
+				t.Errorf("err expected and not received.")
+				return
+			} else if !tc.wantErr && err != nil {
+				t.Errorf("got unexpected error: %v:", err)
+				return
+			} else if tc.wantErr {
+				// Successfully invoked error condition
+				return
+			}
+
+			if tc.want != *got {
+				t.Errorf("want %v, got %v", tc.want, got)
+			}
+		})
+	}
+}
+
+func TestShiftValidate(t *testing.T) {
+	for _, tc := range []struct {
+		desc    string
+		shift   *Shift
+		wantErr bool
+	}{
+		{
+			desc:    "nil shift",
+			shift:   nil,
+			wantErr: true,
+		},
+		{
+			desc:    "empty shift",
+			shift:   &Shift{},
+			wantErr: true,
+		},
+		{
+			desc: "no user",
+			shift: &Shift{
+				StartDate: time.Date(2020, 6, 1, 0, 0, 0, 0, time.UTC),
+			},
+			wantErr: true,
+		},
+		{
+			desc: "no startDate",
+			shift: &Shift{
+				User: "foo",
+			},
+			wantErr: true,
+		},
+		{
+			desc: "stop before start",
+			shift: &Shift{
+				User:      "foo",
+				StartDate: time.Date(2020, 6, 1, 0, 0, 0, 0, time.UTC),
+				StopDate:  time.Date(2020, 5, 1, 0, 0, 0, 0, time.UTC),
+			},
+			wantErr: true,
+		},
+		{
+			desc: "valid shift",
+			shift: &Shift{
+				StartDate:    time.Date(2020, 6, 1, 0, 0, 0, 0, time.UTC),
+				StopDate:     time.Date(2020, 6, 1, 0, 0, 0, 0, time.UTC),
+				User:         "foo",
+				UserOverride: "bar",
+			},
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			err := tc.shift.Validate()
+
+			if tc.wantErr && err == nil {
+				t.Errorf("err expected and not received.")
+				return
+			} else if !tc.wantErr && err != nil {
+				t.Errorf("got unexpected error: %v:", err)
+				return
+			} else if tc.wantErr {
+				// Successfully invoked error condition
+				return
+			}
+		})
+	}
+}
