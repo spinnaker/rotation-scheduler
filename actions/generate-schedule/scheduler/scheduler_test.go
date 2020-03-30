ef5dc1c4b0dfc2ad427ddc70e65ceac5dc52733a
Travis Tomsu <ttomsu@google.com>
2020-03-30 09:21:47 -0400

    Replace proto structs with handcrafted ones (#5)
    * Add non-proto based structs libraries for easier use and readability
    
    * Remove all protos, make existing libraries work with new structs
    
    * ez comments
    
    * plumpy comments

--- actions/generate-schedule/scheduler/scheduler_test.go
+++ actions/generate-schedule/scheduler/scheduler_test.go
@@ -1,12 +1,12 @@
 package scheduler
 
 import (
-	"github.com/ghodss/yaml"
-	"github.com/golang/protobuf/proto"
-	"github.com/spinnaker/rotation-scheduler/actions/generate-schedule/users"
-	"github.com/spinnaker/rotation-scheduler/proto/schedule"
+	"reflect"
 	"testing"
 	"time"
+
+	"github.com/spinnaker/rotation-scheduler/actions/generate-schedule/users"
+	"github.com/spinnaker/rotation-scheduler/schedule"
 )
 
 func TestNewScheduler(t *testing.T) {
@@ -20,178 +20,224 @@ func TestNewScheduler(t *testing.T) {
 }
 
 func TestSchedule(t *testing.T) {
-	s, err := NewScheduler(users.NewStaticSource("first", "second", "third"), 1)
-	if err != nil {
-		t.Fatalf("error creating scheduler: %v", err)
-	}
-
 	for _, tc := range []struct {
-		desc    string
-		start   time.Time
-		stop    time.Time
-		wantErr bool
-		want    *schedule.Schedule
+		desc          string
+		shiftDuration int
+		start         time.Time
+		stop          time.Time
+		wantErr       bool
+		want          *schedule.Schedule
 	}{
 		{
-			desc:    "happy path",
-			start:   time.Date(2020, 01, 01, 0, 0, 0, 0, time.UTC),
-			stop:    time.Date(2020, 01, 04, 0, 0, 0, 0, time.UTC),
-			wantErr: false,
+			desc:          "same day start and stop",
+			shiftDuration: 1,
+			start:         time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+			stop:          time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
 			want: &schedule.Schedule{
 				Shifts: []*schedule.Shift{
 					{
-						StartDate: "Wed 01 Jan 2020",
+						StartDate: time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+						StopDate:  time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+						User:      "first",
+					},
+				},
+			},
+		},
+		{
+			desc:          "same day start and stop, longer shift duration",
+			shiftDuration: 7,
+			start:         time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+			stop:          time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+			wantErr:       true,
+		},
+		{
+			desc:          "happy path",
+			shiftDuration: 1,
+			start:         time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+			stop:          time.Date(2020, 1, 4, 0, 0, 0, 0, time.UTC),
+			want: &schedule.Schedule{
+				Shifts: []*schedule.Shift{
+					{
+						StartDate: time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
 						User:      "first",
 					},
 					{
-						StartDate: "Thu 02 Jan 2020",
+						StartDate: time.Date(2020, 1, 2, 0, 0, 0, 0, time.UTC),
 						User:      "second",
 					},
 					{
-						StartDate: "Fri 03 Jan 2020",
+						StartDate: time.Date(2020, 1, 3, 0, 0, 0, 0, time.UTC),
 						User:      "third",
 					},
-				},
-			},
-		},
-		{
-			desc:    "start after stop",
-			start:   time.Date(2020, 01, 04, 0, 0, 0, 0, time.UTC),
-			stop:    time.Date(2020, 01, 01, 0, 0, 0, 0, time.UTC),
-			wantErr: false,
-			want: &schedule.Schedule{
-				Shifts: []*schedule.Shift{
 					{
-						StartDate: "Sat 04 Jan 2020",
+						StartDate: time.Date(2020, 1, 4, 0, 0, 0, 0, time.UTC),
+						StopDate:  time.Date(2020, 1, 4, 0, 0, 0, 0, time.UTC),
 						User:      "first",
 					},
 				},
 			},
 		},
+		{
+			desc:          "start after stop",
+			shiftDuration: 1,
+			start:         time.Date(2020, 1, 4, 0, 0, 0, 0, time.UTC),
+			stop:          time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+			wantErr:       true,
+		},
+		{
+			desc:          "zero start",
+			shiftDuration: 1,
+			start:         time.Time{},
+			stop:          time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+			wantErr:       true,
+		}, {
+			desc:          "zero stop",
+			shiftDuration: 1,
+			start:         time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+			stop:          time.Time{},
+			wantErr:       true,
+		},
 	} {
 		t.Run(tc.desc, func(t *testing.T) {
+			s, err := NewScheduler(users.NewStaticSource("first", "second", "third"), tc.shiftDuration)
+			if err != nil {
+				t.Fatalf("error creating scheduler: %v", err)
+			}
+
 			got, err := s.Schedule(tc.start, tc.stop)
 			if tc.wantErr && err == nil {
 				t.Errorf("err expected and not received.")
+				return
 			} else if !tc.wantErr && err != nil {
 				t.Errorf("got error from Schedule: %v:", err)
+				return
 			} else if tc.wantErr {
 				// Successfully invoked error condition
 				return
 			}
 
-			if !proto.Equal(tc.want, got) {
-				wantStr := scheduleToString(tc.want, t)
-				gotStr := scheduleToString(got, t)
-				t.Errorf("got schedule different from expected.\nWant:\n%v\n\nGot:\n%v\n", wantStr, gotStr)
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Errorf("got schedule different from expected.\nWant:\n%v\n\nGot:\n%v\n", tc.want, got)
 			}
 		})
 	}
 }
 
 func TestExtendSchedule(t *testing.T) {
-	s, err := NewScheduler(users.NewStaticSource("first", "second", "third"), 1)
-	if err != nil {
-		t.Fatalf("error creating scheduler: %v", err)
-	}
-
-	prevSchedule := &schedule.Schedule{
-		Shifts: []*schedule.Shift{
-			{
-				StartDate: "Wed 01 Jan 2020",
-				User:      "first",
-			},
-			{
-				StartDate: "Thu 02 Jan 2020",
-				User:      "second",
-			},
-		},
-	}
-
-	newSchedule := &schedule.Schedule{
-		Shifts: []*schedule.Shift{
-			{
-				StartDate: "Wed 01 Jan 2020",
-				User:      "first",
-			},
-			{
-				StartDate: "Thu 02 Jan 2020",
-				User:      "second",
-			},
-			{
-				StartDate: "Fri 03 Jan 2020",
-				User:      "third",
-			},
-			{
-				StartDate: "Sat 04 Jan 2020",
-				User:      "first",
-			},
-		},
-	}
-
 	for _, tc := range []struct {
-		desc    string
-		input   *schedule.Schedule
-		stop    time.Time
-		wantErr bool
-		want    *schedule.Schedule
+		desc         string
+		input        *schedule.Schedule
+		durationDays int
+		stop         time.Time
+		wantErr      bool
+		want         *schedule.Schedule
 	}{
 		{
-			desc:    "happy path",
-			input:   prevSchedule,
-			stop:    time.Date(2020, 01, 05, 0, 0, 0, 0, time.UTC),
-			wantErr: false,
-			want:    newSchedule,
+			desc: "single day extension",
+			input: &schedule.Schedule{
+				Shifts: []*schedule.Shift{
+					{
+						StartDate: time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+						User:      "first",
+					},
+					{
+						StartDate: time.Date(2020, 1, 2, 0, 0, 0, 0, time.UTC),
+						StopDate:  time.Date(2020, 1, 2, 0, 0, 0, 0, time.UTC),
+						User:      "second",
+					},
+				},
+			},
+			durationDays: 1,
+			stop:         time.Date(2020, 1, 5, 0, 0, 0, 0, time.UTC),
+			wantErr:      false,
+			want: &schedule.Schedule{
+				Shifts: []*schedule.Shift{
+					{
+						StartDate: time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+						User:      "first",
+					},
+					{
+						StartDate: time.Date(2020, 1, 2, 0, 0, 0, 0, time.UTC),
+						User:      "second",
+					},
+					{
+						StartDate: time.Date(2020, 1, 3, 0, 0, 0, 0, time.UTC),
+						User:      "third",
+					},
+					{
+						StartDate: time.Date(2020, 1, 4, 0, 0, 0, 0, time.UTC),
+						User:      "first",
+					},
+					{
+						StartDate: time.Date(2020, 1, 5, 0, 0, 0, 0, time.UTC),
+						StopDate:  time.Date(2020, 1, 5, 0, 0, 0, 0, time.UTC),
+						User:      "second",
+					},
+				},
+			},
+		},
+		{
+			desc: "multi-day extension",
+			input: &schedule.Schedule{
+				Shifts: []*schedule.Shift{
+					{
+						StartDate: time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+						User:      "first",
+					},
+					{
+						StartDate: time.Date(2020, 1, 8, 0, 0, 0, 0, time.UTC),
+						StopDate:  time.Date(2020, 1, 14, 0, 0, 0, 0, time.UTC),
+						User:      "second",
+					},
+				},
+			},
+			durationDays: 7,
+			stop:         time.Date(2020, 2, 1, 0, 0, 0, 0, time.UTC),
+			wantErr:      false,
+			want: &schedule.Schedule{
+				Shifts: []*schedule.Shift{
+					{
+						StartDate: time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+						User:      "first",
+					},
+					{
+						StartDate: time.Date(2020, 1, 8, 0, 0, 0, 0, time.UTC),
+						User:      "second",
+					},
+					{
+						StartDate: time.Date(2020, 1, 15, 0, 0, 0, 0, time.UTC),
+						User:      "third",
+					},
+					{
+						StartDate: time.Date(2020, 1, 22, 0, 0, 0, 0, time.UTC),
+						StopDate:  time.Date(2020, 1, 28, 0, 0, 0, 0, time.UTC),
+						User:      "first",
+					},
+				},
+			},
 		},
 	} {
 		t.Run(tc.desc, func(t *testing.T) {
-			err := s.ExtendSchedule(tc.input, tc.stop)
+			s, err := NewScheduler(users.NewStaticSource("first", "second", "third"), tc.durationDays)
+			if err != nil {
+				t.Fatalf("error creating scheduler: %v", err)
+			}
+
+			err = s.ExtendSchedule(tc.input, tc.stop)
 			if tc.wantErr && err == nil {
 				t.Errorf("err expected and not received.")
+				return
 			} else if !tc.wantErr && err != nil {
 				t.Errorf("got error from Schedule: %v:", err)
+				return
 			} else if tc.wantErr {
 				// Successfully invoked error condition
 				return
 			}
 
-			if !proto.Equal(tc.want, tc.input) {
-				wantStr := scheduleToString(tc.want, t)
-				gotStr := scheduleToString(tc.input, t)
-				t.Errorf("got schedule different from expected.\nWant:\n%v\n\nGot:\n%v\n", wantStr, gotStr)
+			if !reflect.DeepEqual(tc.want, tc.input) {
+				t.Errorf("got schedule different from expected.\nWant:\n%v\n\nGot:\n%v\n", tc.want, tc.input)
 			}
 		})
 	}
-}
-
-func TestValidatePreviousShifts(t *testing.T) {
-	shifts := []*schedule.Shift{
-		{
-			StartDate: "foobar",
-		},
-	}
-
-	if err := validatePreviousShifts(shifts); err == nil {
-		t.Errorf("expected error from invalid date")
-	}
-
-	shifts[0].StartDate = time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC).Format(DateFormat)
-
-	if err := validatePreviousShifts(shifts); err == nil {
-		t.Errorf("expected error from missing user")
-	}
-}
-
-func scheduleToString(s *schedule.Schedule, t *testing.T) string {
-	j, err := s.MarshalJSON()
-	if err != nil {
-		t.Fatalf("json marshalling: %v", err)
-	}
-
-	b, err := yaml.JSONToYAML(j)
-	if err != nil {
-		t.Fatalf("marshalling: %v", err)
-	}
-
-	return string(b)
 }
