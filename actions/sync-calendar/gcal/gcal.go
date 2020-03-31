cf8d0f35131879794a01dde2383f9ba932acf0fa
Travis Tomsu <ttomsu@google.com>
2020-03-31 12:43:19 -0400

    merge regen_replay into main

--- actions/sync-calendar/gcal/gcal.go
+++ actions/sync-calendar/gcal/gcal.go
@@ -4,13 +4,11 @@ package gcal
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/spinnaker/rotation-scheduler/schedule"
-	"golang.org/x/oauth2/google"
 	"google.golang.org/api/calendar/v3"
 	"google.golang.org/api/option"
 )
@@ -47,26 +45,6 @@ func NewGCal(calendarID string, client *http.Client) (*GCal, error) {
 		CalendarID: calendarID,
 		svc:        svc,
 	}, nil
-}
-
-// Client creates an authenticated HTTP Client from the specified service account credentials. The service account
-// must have a Domain-wide Delegation to control user calendars. See
-// https://developers.google.com/admin-sdk/directory/v1/guides/delegation
-func Client(calendarID, jsonCredentialsPath string) (*http.Client, error) {
-	key, err := ioutil.ReadFile(jsonCredentialsPath)
-	if err != nil {
-		return nil, fmt.Errorf("unable to read JSON credential file: %v", err)
-	}
-	jwtConfig, err := google.JWTConfigFromJSON(key, calendar.CalendarScope)
-	if err != nil {
-		return nil, fmt.Errorf("unable to generate config from JSON credential: %v", err)
-	}
-	// Since apparently service accounts don't have any associated quotas in GSuite,
-	// we must supply a user to charge quota against, and I think they need to have
-	// admin permission on the G Suite account to work.
-	jwtConfig.Subject = calendarID
-
-	return jwtConfig.Client(context.Background()), nil
 }
 
 // Clears all events on the calendar and replaces them with events from Schedule sched.
--- actions/sync-calendar/main.go
+++ actions/sync-calendar/main.go
@@ -1,21 +1,31 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 
+	"cloud.google.com/go/httpreplay"
 	"github.com/ghodss/yaml"
 	"github.com/spinnaker/rotation-scheduler/actions/sync-calendar/gcal"
 	"github.com/spinnaker/rotation-scheduler/schedule"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/calendar/v3"
+	"google.golang.org/api/option"
 )
 
 var (
-	schedPath  = flag.String("schedule", "", "Required. Path to schedule YAML file.")
-	jsonKey    = flag.String("jsonKey", "", "Required. The path to the JSON key with access to the Calendar API.")
-	calendarID = flag.String("calendarID", "build-cop@spinnaker.io", "Optional. The calendar ID to update. Defaults to build-cop@spinnaker.io")
+	schedPath   = flag.String("schedule", "", "Required. Path to schedule YAML file.")
+	jsonKeyPath = flag.String("jsonKey", "", "Required. The path to the JSON key with access to the Calendar API.")
+	calendarID  = flag.String("calendarID", "spinbot@spinnaker.io", "Optional. The calendar ID to update. Defaults to spinbot@spinnaker.io, as that is the preferred test account.")
+
+	record  = flag.Bool("record", false, "Record the responses from external dependencies to a file.")
+	outPath = flag.String("out", "", "Filepath for saved HTTP responses.")
 )
 
 func main() {
@@ -25,10 +35,15 @@ func main() {
 		log.Fatalf("Error validating flags: %v", err)
 	}
 
-	client, err := gcal.Client(*calendarID, *jsonKey)
+	client, closer, err := client()
 	if err != nil {
-		log.Fatalf("Error initializing GCal client: %v", err)
+		log.Fatalf("Error initializing HTTP client: %v", err)
 	}
+	defer func() {
+		if closer != nil {
+			_ = closer.Close()
+		}
+	}()
 
 	cal, err := gcal.NewGCal(*calendarID, client)
 	if err != nil {
@@ -48,6 +63,39 @@ func main() {
 	if err := cal.Schedule(sched); err != nil {
 		log.Fatalf("Error syncing schedule: %v", err)
 	}
+
+}
+
+func client() (*http.Client, io.Closer, error) {
+	key, err := ioutil.ReadFile(*jsonKeyPath)
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to read JSON credential file: %v", err)
+	}
+	jwtConfig, err := google.JWTConfigFromJSON(key, calendar.CalendarScope)
+	if err != nil {
+		return nil, nil, fmt.Errorf("unable to generate config from JSON credential: %v", err)
+	}
+	// Since apparently service accounts don't have any associated quotas in GSuite,
+	// we must supply a user to charge quota against, and I think they need to have
+	// admin permission on the G Suite account to work.
+	jwtConfig.Subject = *calendarID
+	ctx := context.Background()
+
+	if *record {
+		r, err := httpreplay.NewRecorder(*outPath, []byte{})
+		if err != nil {
+			return nil, nil, fmt.Errorf("error intializing recorder: %v", err)
+		}
+
+		// Can't use `option.WithHTTPClient` here because the library throws an error when it already has a client.
+		client, err := r.Client(ctx, option.WithTokenSource(jwtConfig.TokenSource(ctx)))
+		if err != nil {
+			return nil, nil, fmt.Errorf("error creating recorder client: %v", err)
+		}
+		return client, r, nil
+	}
+
+	return jwtConfig.Client(ctx), nil, nil
 }
 
 func validateFlags() error {
@@ -59,12 +107,12 @@ func validateFlags() error {
 		return fmt.Errorf("schedule must be a file, got a directory")
 	}
 
-	if *jsonKey == "" {
-		return fmt.Errorf("--jsonKey flag is required and must not be empty")
-	} else if info, err := os.Stat(*jsonKey); os.IsNotExist(err) {
-		return fmt.Errorf("jsonKey file (%v) not found: %v", *jsonKey, err)
+	if *jsonKeyPath == "" {
+		return fmt.Errorf("--jsonKeyPath flag is required and must not be empty")
+	} else if info, err := os.Stat(*jsonKeyPath); os.IsNotExist(err) {
+		return fmt.Errorf("jsonKeyPath file (%v) not found: %v", *jsonKeyPath, err)
 	} else if info.IsDir() {
-		return fmt.Errorf("jsonKey must be a file, got a directory")
+		return fmt.Errorf("jsonKeyPath must be a file, got a directory")
 	}
 
 	return nil
